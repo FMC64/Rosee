0d67e91d6a92a0abf57f565adceb6694ad0edbba
Adrien LENGLET <adrien.lenglet3@gmail.com>
2020-11-14 19:10:18 +0100

    Add Rosee::vector, an efficient C-based vector template

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include "Rosee/vector.hpp"
+
 int main(void)
 {
+	/*Rosee::vector<size_t> vec {1, 2, 3};
+
+	vec.emplace(5);
+	vec.emplace(8);
+	vec.emplace(1);
+	vec.emplace(2);
+	vec.emplace(3);*/
+	/*vec.erase(std::remove_if(vec.begin(), vec.end(), [](auto &v){
+		return v >= 3;
+	}), vec.end());*/
+	//vec.erase(vec.begin());
+	//vec.insert(vec.begin() + 1, 5ULL);
+
+	auto vec = Rosee::vector<size_t>(4, 58);
+	for (auto &v : vec)
+		std::cout << v << std::endl;
 	return 0;
 }
