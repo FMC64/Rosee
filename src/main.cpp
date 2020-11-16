f69c742e11f7e4214a77ce05d1277104fd3bd53e
Adrien LENGLET <adrien.lenglet3@gmail.com>
2020-11-16 12:40:06 +0100

    Put tabs instead of spaces in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 using namespace Rosee;
 
 int main() {
-        auto b = Brush(Cmp::make_id_array<Id, Transform>());
-        auto ent = b.add(64000);
-        auto ids = b.get<Id>();
-        std::cout << "ent id: " << ent << ", id storage: " << ids << std::endl;
-        b.remove(ent, 64000);
-        return 0;
+	auto b = Brush(Cmp::make_id_array<Id, Transform>());
+	auto ent = b.add(64000);
+	auto ids = b.get<Id>();
+	std::cout << "ent id: " << ent << ", id storage: " << ids << std::endl;
+	b.remove(ent, 64000);
+	return 0;
 }
